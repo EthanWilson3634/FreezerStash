82751390aeba2aae269f040415692b401bd81fad
Ethan <ethanwilson008@gmail.com>
2025-03-16 21:14:37 -0400

    Force light mode and thats a wrap

--- Breastmilk Tracker/Breastmilk_TrackerApp.swift
+++ Breastmilk Tracker/Breastmilk_TrackerApp.swift
@@ -2,19 +2,9 @@ import SwiftUI
 
 @main
 struct BreastmilkTrackerApp: App {
-    // Override the interface style to light mode on app launch
-    init() {
-        // Ensure the app starts in light mode
-        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
-            windowScene.windows.forEach { window in
-                window.overrideUserInterfaceStyle = .light
-            }
-        }
-    }
-
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().preferredColorScheme(.light)
         }
     }
 }
